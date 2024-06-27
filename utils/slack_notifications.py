c9f3786869905e8746d0a3e6816089dd30763a92
Jack Conway <jackconwayiv@gmail.com>
2024-06-27 13:36:36 -0700

    refactor to avoid signals

--- utils/slack_notifications.py
+++ utils/slack_notifications.py
@@ -1,7 +1,5 @@
-# utils/slack_notifications.py
 from django.conf import settings
 from slack_sdk.webhook import WebhookClient
-
 
 def send_slack_message(text):
     webhook_url = settings.SLACK_WEBHOOK_URL
